605d27f4691cad394f8bc4e332aef9761026e849
Bilas Yurii <bilas.yurii@gmail.com>
2019-12-12 20:39:50 +0200

    Added IEntity and Place entity.

---
+++ api/Geolocation.Core/Entities/Place.cs
@@ -0,0 +1,11 @@
+﻿namespace Geolocation.Core.Entities
+{
+    public class Place : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ api/Geolocation/Controllers/PlaceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geolocation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlaceController : ControllerBase
+    {
+        public PlaceController() { }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return null;
+            /*
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();*/
+        }
+    }
+}
