cd421a429bcc861575576d1ab79b2f2aa9665a7a
Bilas Yurii <bilas.yurii@gmail.com>
2019-12-12 22:02:48 +0200

    Added IPlaceService to Core.

---
+++ api/Geolocation.Core/Abstractions/Services/IPlaceService.cs
@@ -0,0 +1,14 @@
+﻿using Geolocation.Core.Entities;
+using System.Collections.Generic;
+
+namespace Geolocation.Core.Abstractions.Services
+{
+    public interface IPlaceService<TId>
+    {
+        public List<Place> Get();
+        public Place GetById(TId id);
+        public Place Insert(Place user);
+        public Place Update(Place user);
+        public void Delete(TId id);
+    }
+}
