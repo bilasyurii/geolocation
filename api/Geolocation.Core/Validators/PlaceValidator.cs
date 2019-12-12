2e8b1da8e2a3fdc30fde3c182f9029d035f56567
Bilas Yurii <bilas.yurii@gmail.com>
2019-12-12 22:11:48 +0200

    Added a validator.

---
+++ api/Geolocation.Core/Validators/PlaceValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Geolocation.Core.Entities;
+
+namespace Geolocation.Core.Validators
+{
+    public class PlaceValidator : AbstractValidator<Place>
+    {
+        public PlaceValidator()
+        {
+            RuleFor(place => place.Latitude)
+                .Must(lat => lat >= -90 && lat <= 90)
+                .WithMessage("Latitude must be in range [-90;90] (inclusive).");
+            RuleFor(place => place.Longitude)
+                .Must(lng => lng >= -180 && lng <= 180)
+                .WithMessage("Longitude must be in range [-180;180] (inclusive).");
+        }
+    }
+}
