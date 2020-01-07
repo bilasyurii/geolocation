8931f47747203c344b722501b160f2c2bfe6ac82
Bilas Yurii <bilas.yurii@gmail.com>
2020-01-07 22:53:42 +0200

    Added title changing.

--- web/src/app/components/app/app.component.ts
+++ web/src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 import { PlacesService } from 'src/app/services/places.service';
 
@@ -9,4 +11,27 @@ import { PlacesService } from 'src/app/services/places.service';
   styleUrls: ['./app.component.scss'],
   providers: [PlacesService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  private appName = 'Geolocation';
+
+  constructor(private router: Router, private titleService: Title) { }
+
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            const url = (event as NavigationEnd).urlAfterRedirects;
+            let title = this.appName;
+            if (url === '/places') {
+              title = 'Places | ' + title;
+            } else if (url === '/map') {
+              title = 'Map | ' + title;
+            } else if (url === '/404') {
+              title = 'Page not found | ' + title;
+            }
+            this.titleService.setTitle(title);
+          }
+        }
+      );
+  }
+}
